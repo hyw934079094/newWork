7c3f85a22fe4a2be2df60f7ae5eb00784be6c0d1
hyw934079094 <34641046+hyw934079094@users.noreply.github.com>
2026-08-12 20:43:54 +0800

    chore: add story-admin-server/src/main/java/com/story/admin/StoryAdminApplication.java

---
+++ story-admin-server/src/main/java/com/story/admin/StoryAdminApplication.java
@@ -0,0 +1,5 @@
+package com.story.admin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class StoryAdminApplication { public static void main(String[] args) { SpringApplication.run(StoryAdminApplication.class, args); } }
