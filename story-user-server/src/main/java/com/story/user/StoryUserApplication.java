0b9ad1e5b0c198e4d29badedd3aa89e32321083f
hyw934079094 <34641046+hyw934079094@users.noreply.github.com>
2026-08-12 20:44:08 +0800

    chore: add story-user-server/src/main/java/com/story/user/StoryUserApplication.java

---
+++ story-user-server/src/main/java/com/story/user/StoryUserApplication.java
@@ -0,0 +1,5 @@
+package com.story.user;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class StoryUserApplication { public static void main(String[] args) { SpringApplication.run(StoryUserApplication.class, args); } }
